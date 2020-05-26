9157eba9b230e8382f9b995e78d6f47f6292cb07
SB2211 <suyash.batham_cs18@gla.ac.in>
2020-05-26 12:08:41 +0530

    Hangman Game

---
+++ Algorithm.py
@@ -0,0 +1,10 @@
+"""
+1.Develop the interface
+2.predefined list
+3.instruction
+4.check the words
+  if right then enter in empty list
+  then draw figure
+5.reduce the turns
+6.generate the figure
+"""
